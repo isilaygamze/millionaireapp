a72a3ccc4dc16f7e295d40a8c0dee24c5438a0fd
isilaygamze <igtanchor1@gmail.com>
2023-04-24 22:35:58 +0300

    feat:app added.

---
+++ quiz/src/components/Trivia.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export default function Trivia({
+    data, 
+    setStop, 
+    questionNumber, 
+    setQuestionNumber
+}) {
+
+    const[question,setQestion]=useState(null)
+    const[selectedAnswer, setSelectedAnswer]=useState(null)
+    const[className,setClassName]=useState("answer")
+
+    useEffect(()=>{
+        setQestion(data[questionNumber-1])
+    },[data,questionNumber])
+
+    const delay = (duration,callback)=>{
+    setTimeout(()=>{
+                    callback();
+                   },duration);
+    }
+
+    const handleClick=(a)=>{
+        setSelectedAnswer(a);
+        setClassName("answer active");
+        delay(3000,()=>{
+            setClassName(a.correct ? "answer correct" : "answer wrong")
+        });
+        delay(6000,()=>{
+            if(a.correct){
+                setQuestionNumber((prev)=>prev+1);
+                setSelectedAnswer(null);
+            }
+            else{
+                setStop(true);
+            }
+        });
+
+    }
+
+
+    return(
+        <div className="trivia">
+            <div className="question">{question?.question}</div>
+            <div className="answers">{question?.answers.map((a)=>(
+                <div className={selectedAnswer === a ? className:"answer"} onClick={()=>handleClick(a)}>{a.text}</div>
+            ))}
+                
+                
+            </div>
+        </div>
+    )
+}
